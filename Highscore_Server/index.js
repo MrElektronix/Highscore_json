2b97bf798cf9ffa8f71ac58604054e7fec751097
MrElektronix <milovanpelt@gmail.com>
2018-07-05 15:39:58 +0200

    test png file

--- Highscore_Server/index.js
+++ Highscore_Server/index.js
@@ -49,7 +49,7 @@ app.get("/Highscore_Table", (req, res)=>{
 				//return b.score - a.score;
 				return ('' + b.score).localeCompare(a.score);
 			});
-			res.render('highscore.ejs', {scoreArray: scores, teamNameArray: scores, image: "http://5.157.85.78:2000/images/escape.png"});
+			res.render('highscore.ejs', {scoreArray: scores, teamNameArray: scores, image: "http://5.157.85.78:2000/images/best_meme.png"});
 		} else{
 			res.render('highscore.ejs', {scoreArray: "", teamNameArray: ""});
 		}
