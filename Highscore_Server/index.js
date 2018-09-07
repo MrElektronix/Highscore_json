4302aa67d4bed920233019de7e87732677e77b80
MrElektronix <milovanpelt@gmail.com>
2018-09-07 17:21:00 +0200

    remove

--- Highscore_Server/index.js
+++ Highscore_Server/index.js
@@ -114,8 +114,8 @@ io.on("connection", (socket)=>{
 		RemoveSchemaData(HighscoreSchema);
 		RemoveSchemaData(DaySchema);
 		RemoveSchemaData(ImageSchema);
-		DeleteLocalImage("escape0.jpg");
-		DeleteLocalImage("escape1.jpg");
+		//DeleteLocalImage("escape0.jpg");
+		//DeleteLocalImage("escape1.jpg");
 		DeleteLocalImage("escape2.jpg");
 		
 		//CheckDay(CheckDate());
